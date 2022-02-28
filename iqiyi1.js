26ade085ed09522c56ab1e821fab482ac9977586
csrxff <53332718+csrxff@users.noreply.github.com>
2022-02-28 16:49:08 +0800

    Update iqiyi1.js

--- iqiyi1.js
+++ iqiyi1.js
@@ -90,7 +90,7 @@ var $nobyda = nobyda();
 	if (typeof process !== 'undefined' && typeof process.env !== 'undefined') {
 		cookie = cookie || process.env.IQIYI1_COOKIE;
 		LogDetails = LogDetails || process.env.IQIYI_DEBUG;
-		barkKey = barkKey || process.env.BARK_PUSE;
+		barkKey = barkKey || process.env.BARK1;
 		barkServer = barkServer || process.env.BARK_SERVER;
 	}
   if ($nobyda.isRequest) {

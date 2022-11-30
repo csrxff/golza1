fdf21ee7d49ab3183d292ab6df9136407a994f7d
csrxff <53332718+csrxff@users.noreply.github.com>
2022-11-30 11:49:17 +0800

    Update telecom_live_lotter.py

--- telecom_live_lotter.py
+++ telecom_live_lotter.py
@@ -9,8 +9,7 @@
 """
 1. 脚本仅供学习交流使用, 请在下载后24h内删除
 2. 环境变量说明:
-    export TELECOM_LOTTERY="号码&密码" 
-    多账号回车
+    export TELECOM_LOTTERY = 手机号1@密码1换行手机号2@密码2
 3. 必须登录过 电信营业厅 app的账号才能正常运行
 """
 from re import findall
@@ -214,7 +213,7 @@ def get_data():
 def main(phone, password):
     apiType = 1
     try:
-        url = "https://gitee.com/kele2233/genxin/raw/master/telecomLiveInfo.json"
+        url = "https://raw.githubusercontent.com/limoruirui/Hello-World/main/telecomLiveInfo.json"
         data = get(url, timeout=5).json()        
     except:
         data = getData
